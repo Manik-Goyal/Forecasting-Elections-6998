a7c6e43590b416e5f965a2d239d61a7be700bf0a
md3507 <md3507@columbia.edu>
2020-10-29 16:07:59 -0700

    dataframe mutations started

--- scripts/model/final_2016.py
+++ scripts/model/final_2016.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 
 
+
+NaN = float('nan')
+
+
 def cov_matrix(n, sigma2, rho):
 	m = np.ones(shape=(n,n)) * rho
 	m2 = np.zeros(shape=(n,n))
@@ -21,15 +25,37 @@ def mean_low(draws, states, identity):
 #def check_cov_matrix(matrix, wt=state_weights):
 
 
+def read_all_polls():
+	cols = ['state', 'pollster', 'number.of.observations','population', 'mode', 
+		'start.date', 
+		'end.date',
+		'clinton', 'trump', 'undecided', 'other', 'johnson', 'mcmullin']
+	df = pd.read_csv("../../data/all_polls.csv", usecols=cols)
+	pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+	# Mutations
+
+	# Matching equivalent names due to data inconsistencies
+	df = df.replace("Fox News", "FOX")
+	df = df.replace("WashPost", "Washington Post")
+	df = df.replace("ABC News", "ABC")
+	df = df.replace("DHM Research", "DHM")
+	df = df.replace("Public Opinion Strategies", "POS")
+	df['undecided'] = df['undecided'].replace(NaN, 0)
+	df['other'] = df['other'].replace(NaN, 0) + df['johnson'].replace(NaN, 0) + df['mcmullin'].replace(NaN, 0)
+
 
 
 def main():
 	print("Example usages below for undertanding the code: \n\n")
 	print("Using cov_matrix(6, .75, .95): \n", cov_matrix(6, 0.75, 0.95), '\n\n')
 
-	df = pd.read_csv("../../data/all_polls.csv")
-	df = df.drop(df.columns[0] , axis=1)
-	print(df)
+	read_all_polls()
+
+
+
+#	state_covariance_polling_bias = cov_matrix(51, 0.078^2, 0.9) # 3.4% on elec day
+#	state_covariance_polling_bias <- state_covariance_polling_bias * state_correlation_polling
 
 
 if __name__ == "__main__":
