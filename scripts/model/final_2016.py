57f7f36cbdb7c87941ff7ac7fb05b8b1b3340657
md3507 <md3507@columbia.edu>
2020-10-29 21:13:51 -0700

    more data cleaning; adding differences between days

--- scripts/model/final_2016.py
+++ scripts/model/final_2016.py
@@ -1,11 +1,10 @@
 import numpy as np
 import pandas as pd
+from datetime import datetime
+from datetime import timedelta
 
-
-
+ 
 NaN = float('nan')
-
-
 def cov_matrix(n, sigma2, rho):
 	m = np.ones(shape=(n,n)) * rho
 	m2 = np.zeros(shape=(n,n))
@@ -15,14 +14,6 @@ def cov_matrix(n, sigma2, rho):
 	
 	return(np.matmul(np.matmul(m2, m), m2))
 
-'''
-def mean_low(draws, states, identity):
-	temp = draws
-	draws_df 
-'''
-
-
-#def check_cov_matrix(matrix, wt=state_weights):
 
 
 def read_all_polls():
@@ -32,9 +23,12 @@ def read_all_polls():
 		'clinton', 'trump', 'undecided', 'other', 'johnson', 'mcmullin']
 	df = pd.read_csv("../../data/all_polls.csv", usecols=cols)
 #	pd.set_option("display.max_rows", None, "display.max_columns", None)
-
+#a + (b - a)/2
 	# Mutations
 	df = df.rename(columns={"number.of.observations": "n", "population": "polltype"})
+	df['begin'] = [datetime.strptime(x, "%Y-%m-%d") for x in df['start.date']]
+	df['end'] = [datetime.strptime(x, "%Y-%m-%d") for x in df['end.date']]
+	df['t'] = [(y - (timedelta(days=1) + (y - x) // 2) ).date() for x,y in zip(df['begin'], df['end'])]
 
 	# Matching equivalent names due to data inconsistencies
 	df = df.replace("Fox News", "FOX")
@@ -51,9 +45,18 @@ def read_all_polls():
 	df['pct_clinton'] = df['clinton'] / df['two_party_sum']
 	df['n_trump'] = round(df['n'] * df['trump'] / 100)
 	df['pct_trump'] = df['trump'] / df['two_party_sum']
-	
+
+	# Numerical Indices
+	df['index_s'] = pd.Categorical(df['state']).codes
+	df['index_s'] = df['index_s'].replace(0, 52)
+
+	min_T = df['t'].min()
+	df['index_t'] = [(timedelta(days=1)  + x - min_T).days for x in df['t']]
+
 	return df
 
+#def read_potus_results_76_16(df):
+	
 
 
 def main():
