b0257b171ff2001186ce08d2655fcfeeede912ce
md3507 <md3507@columbia.edu>
2020-11-27 11:44:17 -0800

    passes all flake8 checks

--- scripts/model/final_2016.py
+++ scripts/model/final_2016.py
@@ -162,8 +162,8 @@ def pass_data():
 
     white_evangel = pd.read_csv('../../data/white_evangel_pct.csv')
     white_evangel = white_evangel.dropna()
-    white_evangel = white_evangel.rename(columns=
-                                         {'pct_white_evangel': 'value'})
+    white_evangel = white_evangel.rename(columns={
+                                         'pct_white_evangel': 'value'})
     white_evangel.insert(1, 'variable', ['pct_white_evangel'] *
                          len(white_evangel['state']))
     states = states.append(white_evangel, ignore_index=True)
@@ -177,83 +177,96 @@ def pass_data():
     for i in range(len(indices)):
         states.iloc[i] = (states.iloc[i] - mins[i]) / (maxes[i] - mins[i])
 
-    # Our covariance matrix for the data we have transformed        
+    # Our covariance matrix for the data we have transformed
     cov = states.corr()
-    cov[cov < 0 ] = 0
+    cov[cov < 0] = 0
     cov = cov.to_numpy()
     cov = nearestPD(cov)
-    cov = pd.DataFrame(data = cov, index=state_abbrv, columns=state_abbrv)
+    cov = pd.DataFrame(data=cov, index=state_abbrv, columns=state_abbrv)
     temp_cov = copy.deepcopy(cov)
     np.fill_diagonal(temp_cov.values, float('NaN'))
 
     lamda = 0.75
-    arr = pd.DataFrame(data=np.ones((51,51)), index=state_abbrv, columns=state_abbrv)
-    a = 1
+    arr = pd.DataFrame(data=np.ones((51, 51)),
+                       index=state_abbrv,
+                       columns=state_abbrv)
     state_correlation_polling = lamda * cov + (1 - lamda) * arr
     ind_list = state_abbrv
 
     # covariance matrix for polling error
-    state_covariance_polling_bias =  pd.DataFrame(data=cov_matrix(len(ind_list), 0.078**2, 0.9), columns = ind_list, index=ind_list)
-    state_covariance_polling_bias = state_covariance_polling_bias * state_correlation_polling
+    state_covariance_polling_bias = pd.DataFrame(data=cov_matrix
+                                                 (len(ind_list),
+                                                  0.078**2, 0.9),
+                                                 columns=ind_list,
+                                                 index=ind_list)
+    state_covariance_polling_bias = (state_covariance_polling_bias
+                                     * state_correlation_polling)
 
     # covariance for prior e-day prediction
-    state_covariance_mu_b_T = pd.DataFrame(data=cov_matrix(len(ind_list), 0.18**2, 0.9), columns = ind_list, index=ind_list)
-    state_covariance_mu_b_T = state_covariance_mu_b_T * state_correlation_polling
+    state_covariance_mu_b_T = pd.DataFrame(data=cov_matrix
+                                           (len(ind_list), 0.18**2, 0.9),
+                                           columns=ind_list,
+                                           index=ind_list)
+    state_covariance_mu_b_T = (state_covariance_mu_b_T
+                               * state_correlation_polling)
 
     # covariance matrix for random walks
-    state_covariance_mu_b_walk = pd.DataFrame(cov_matrix(len(ind_list), 0.017**2, 0.9), columns = ind_list, index=ind_list)
-    state_covariance_mu_b_walk = state_covariance_mu_b_walk * state_correlation_polling # we want the demo correlations for filling in gaps in the polls
+    state_covariance_mu_b_walk = pd.DataFrame(cov_matrix
+                                              (len(ind_list), 0.017**2, 0.9),
+                                              columns=ind_list,
+                                              index=ind_list)
+    state_covariance_mu_b_walk = (state_covariance_mu_b_walk
+                                  * state_correlation_polling)
 
-
-    ## MAKE DEFAULT COV MATRICES
+    # MAKE DEFAULT COV MATRICES
     # we're going to make TWO covariance matrix here and pass it
-    # and 3 scaling values to stan, where the 3 values are 
+    # and 3 scaling values to stan, where the 3 values are
     # (1) the national sd on the polls, (2) the national sd
     # on the prior and (3) the national sd of the random walk
     # make initial covariance matrix (using specified correlation)
-    state_covariance_0 = pd.DataFrame(cov_matrix(len(ind_list), 0.07**2, 0.9), columns = ind_list, index=ind_list)
-    state_covariance_0 = state_covariance_0 * state_correlation_polling # we want the demo correlations for filling in gaps in the polls
+    state_covariance_0 = pd.DataFrame(cov_matrix
+                                      (len(ind_list), 0.07**2, 0.9),
+                                      columns=ind_list,
+                                      index=ind_list)
+    state_covariance_0 = state_covariance_0 * state_correlation_polling
 
     # save the inital scaling factor
     state_weights = state_weights.loc[ind_list]
-    national_cov_matrix_error_sd = ( (state_weights.transpose().dot(state_covariance_0)).dot( state_weights ) ) **0.5 # @TODO
+    national_cov_matrix_error_sd = ((state_weights.transpose().dot(
+                                     state_covariance_0)).dot(
+                                     state_weights)) ** 0.5
 
-    days_til_election = [100] # as.numeric(difftime(election_day,RUN_DATE))
+    print(national_cov_matrix_error_sd)
+    days_til_election = [100]
     expected_national_mu_b_T_error = fit_rmse_day_x(days_til_election)[0]
 
-    polling_bias_scale = 0.013 # on the probability scale -- we convert later down
-    mu_b_T_scale = expected_national_mu_b_T_error # on the probability scale -- we convert later down
-    random_walk_scale =  0.05/(300 ** 0.5) # on the probability scale -- we convert later down
-
-    # gen fake matrices, check the math (this is recreated in stan
-    ss_cov_poll_bias = state_covariance_0 * (polling_bias_scale/national_cov_matrix_error_sd*4) ** 2
-    ss_cov_mu_b_T = state_covariance_0 * (mu_b_T_scale/national_cov_matrix_error_sd*4) ** 2
-    ss_cov_mu_b_walk = state_covariance_0 * (random_walk_scale/national_cov_matrix_error_sd*4) ** 2
-
-    check_cov_matrix(ss_cov_poll_bias, state_weights)
-
+    # on the probability scale -- we convert later down
+    polling_bias_scale = 0.013
+    mu_b_T_scale = expected_national_mu_b_T_error
+    random_walk_scale = 0.05/(300 ** 0.5)
 
     cols = ['incvote', 'juneapp', 'q2gdp']
     dfTemp = pd.read_csv("../../data/abramowitz_data.csv", usecols=cols)
 
-    model = smf.ols(formula='incvote ~  juneapp + q2gdp', data = dfTemp) 
+    model = smf.ols(formula='incvote ~ juneapp + q2gdp', data=dfTemp)
     res = model.fit()
     print(res.summary())
     coefs = res.params
-    national_mu_prior =  coefs["Intercept"] + coefs["juneapp"] * 4 + coefs["q2gdp"] * 1.1	
+    national_mu_prior = (coefs["Intercept"]
+                         + coefs["juneapp"] * 4
+                         + coefs["q2gdp"] * 1.1)
 
     # on correct scale
-    national_mu_prior = national_mu_prior / 100 
+    national_mu_prior = national_mu_prior / 100
     # Mean of the mu_b_prior
     mu_b_prior = logit(national_mu_prior + prior_diff_score)
 
-    #@TODO need to fix the df; it's too big for some reason 
     N_state_polls = len(df.loc[df['index_s'] != 51].index)
     N_national_polls = len(df.loc[df['index_s'] == 51].index)
 
     S = len(ind_list)
     P = len(df['pollster'].unique())
-    M = len(df['mode'].unique()) #@TODO switch to 'method' and do mutation
+    M = len(df['mode'].unique())
     Pop = len(df['polltype'].unique())
 
     state = df.loc[df['index_s'] != 51]['index_s'].tolist()
@@ -265,7 +278,6 @@ def pass_data():
     poll_mode_state = df.loc[df['index_s'] != 51]['index_m'].tolist()
     poll_pop_national = df.loc[df['index_s'] == 51]['index_pop'].tolist()
     poll_pop_state = df.loc[df['index_s'] != 51]['index_pop'].tolist()
-
 
     # What we will end up trying to predict
     n_democrat_national = [0 if np.isnan(x) else x for x in
@@ -284,13 +296,11 @@ def pass_data():
                          zip(n_democrat_state, n_republic_state)]
 
     T = (election_day - first_day).days
-    #current_T <- max(df$poll_day)
-
     adjusters = {"ABC", "Washington Post", "Ipsos", "Pew", "YouGov", "NBC"}
     unadjusted_national = [1 if x not in adjusters else 0 for x in
-                            df.loc[df['index_s'] == 51]['pollster'].tolist()]
+                           df.loc[df['index_s'] == 51]['pollster'].tolist()]
     unadjusted_state = [1 if x not in adjusters else 0 for x in
-                         df.loc[df['index_s'] != 51]['pollster'].tolist()]
+                        df.loc[df['index_s'] != 51]['pollster'].tolist()]
 
     # priors (on the logit scale)
     sigma_measure_noise_national = 0.04
@@ -341,6 +351,7 @@ def pass_data():
     data["polling_bias_scale"] = polling_bias_scale
     data["mu_b_T_scale"] = mu_b_T_scale
     data["random_walk_scale"] = random_walk_scale
+    data["ev_state"] = ev_state
 
     g_mu_b_prior.index = data["mu_b_prior"].index
     data["mu_b_prior"] = g_mu_b_prior
