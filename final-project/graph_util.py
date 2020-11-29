1345da32a989fcfdc72266a0066d1c8a68203a89
Manik Goyal <manik@Maniks-MBP.lan>
2020-11-29 09:09:55 -0500

    Reduced WordCount

--- final-project/graph_util.py
+++ final-project/graph_util.py
@@ -1,4 +1,11 @@
 from collections import defaultdict
+from scipy.special import expit
+import numpy as np
+import pandas as pd
+import torch
+from tqdm.notebook import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def average(vals):
@@ -49,3 +56,74 @@ def electoralVoteCalculator(numDays, numStates, preds, EV_Index, EV):
         EV_list_clinton.append(evTotal_clinton)
         EV_list_trump.append(evTotal_trump)
     return clintonWins, trumpWins, EV_list_trump, EV_list_clinton
+
+
+def mean_low_high(draws, states, Id):
+    mean = expit(draws.mean(axis=0))
+    high = expit(draws.mean(axis=0) + 1.96 * draws.std(axis=0))
+    low = expit(draws.mean(axis=0) - 1.96 * draws.std(axis=0))
+    Id = [Id] * len(states)
+    draws_df = {'states': states, 'mean': mean, 'high': high,
+                'low': low, 'type': Id}
+    draws_df = pd.DataFrame(draws_df)
+
+    return draws_df
+
+
+def function_tibble(x, predicted):
+    temp = predicted[:, :, x]
+    low = np.quantile(predicted[:, :, x], 0.025, axis=0)
+    high = np.quantile(predicted[:, :, x], 0.975, axis=0)
+    mean = torch.mean(predicted[:, :, x], axis=0)
+    prob = (predicted[:, :, x] > 0.5).type(torch.float).mean(axis=0)
+    state = [x] * temp.shape[1]
+    t = np.arange(temp.shape[1])
+    df = pd.DataFrame({'low': low, 'high': high, 'mean': mean,
+                       'prob': prob, 'state': state, 't': t})
+
+    return df
+
+
+def generate_mu_sigma(EV_C_mu, EV_C_std, EV_T_mu, EV_T_std,
+                      cWinsList, tWinsList, predicted,
+                      numDays, numStates, EV_Index, EV):
+    for i in tqdm(range(252)):
+        vals = predicted[:, i, :]
+        pred = createPredictionsDict(vals)
+        out = electoralVoteCalculator(numDays, numStates,
+                                      pred, EV_Index, EV)
+        cWins, tWins, EV_T, EV_C = out[0], out[1], out[2], out[3]
+        cWinsList.append(cWins)
+        tWinsList.append(tWins)
+
+        mu_C, mu_T = average(EV_C), average(EV_T)
+        std_C, std_T = std(EV_C, mu_C), std(EV_T, mu_T)
+
+        EV_C_mu.append(mu_C)
+        EV_C_std.append(std_C)
+
+        EV_T_mu.append(mu_T)
+        EV_T_std.append(std_T)
+
+    return EV_C_mu, EV_C_std, EV_T_mu, EV_T_std, \
+        cWinsList, tWinsList
+
+
+def plot_ev_over_time(EV_C_mu, EV_C_std, EV_T_mu, EV_T_std):
+    upper_C = [x + y for x, y in zip(EV_C_mu, EV_C_std)]
+    lower_C = [x - y for x, y in zip(EV_C_mu, EV_C_std)]
+
+    upper_T = [x + y for x, y in zip(EV_T_mu, EV_T_std)]
+    lower_T = [x - y for x, y in zip(EV_T_mu, EV_T_std)]
+
+    sns.lineplot(data=np.array(EV_C_mu), color="blue")
+    sns.lineplot(data=np.array(EV_T_mu), color="red")
+    plt.fill_between(range(len(EV_C_mu)), upper_C, lower_C,
+                     facecolor="blue", alpha=0.25)
+    plt.fill_between(range(len(EV_T_mu)), upper_T, lower_T,
+                     facecolor="red", alpha=0.25)
+    plt.legend(['Clinton', 'Trump'])
+    plt.xlabel("Day")
+    plt.ylabel("Electoral Votes")
+    plt.title("Number of Electoral Votes over Time w. Deviation")
+    plt.show()
